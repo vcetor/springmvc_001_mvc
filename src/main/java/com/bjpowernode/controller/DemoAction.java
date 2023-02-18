a630657879c4cac3177b9f60cd611e7ac7229253
vcetor <931666163@qq.com>
2023-02-18 16:09:58 +0800

    初次提交

---
+++ src/main/java/com/bjpowernode/controller/DemoAction.java
@@ -0,0 +1,23 @@
+package com.bjpowernode.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller  //交给Spring去创建对象
+@RequestMapping("/zar")
+public class DemoAction {
+    /**
+     * action中所有的功能实现都是由方法来完成的
+     * action方法的规范
+     * public
+     * 返回值任意
+     * 名字任意
+     * 参数任意
+     * 需要@RequestMapping()
+     */
+    @RequestMapping("/demo")
+    public String demo(){
+        System.out.println("服务器被访问到了.....");
+        return "main"; //可以直接跳转到/admin/main.jsp页面上
+    }
+}
---
+++ target/springmvc_001_demo-1.0/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2023/2/11
+  Time: 16:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<br><br><br>
+<a href="${pageContext.request.contextPath}/zar/demo.action">访问服务器</a>
+</body>
+</html>
